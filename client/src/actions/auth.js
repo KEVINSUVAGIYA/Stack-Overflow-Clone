3ee949408c39a95f7feee5e3b13f2087a1d4d60e
KEVINSUVAGIYA <kevinsuvagiya11@gmail.com>
2023-01-31 22:15:43 +0530

    3rd

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,5 +1,6 @@
 import * as api from '../api'
-import {setCurrentUser} from './currentUser' 
+import {setCurrentUser} from './currentUser'
+ 
 export const signup = (authData, navigate) => async (dispatch) => {
     try {
         const { data } = await api.signUp(authData)
@@ -16,7 +17,6 @@ export const login = (authData, navigate) => async (dispatch) => {
         const { data } = await api.logIn(authData)
         dispatch({ type: 'AUTH', data})
         dispatch(setCurrentUser( JSON.parse(localStorage.getItem('profile')) ))
-
         navigate('/')
     } catch (error) {
         console.log(error)
