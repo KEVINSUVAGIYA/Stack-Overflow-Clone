35a98ca84c773aea6b543665720cc9c4120437aa
KEVINSUVAGIYA <kevinsuvagiya11@gmail.com>
2023-05-13 21:01:50 +0530

    3rd

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv'
 
 import userRoutes from './routes/users.js'
 import questionRoutes from './routes/Questions.js'
-import answerRoutes from './routes/Answer.js'
+import answerRoutes from './routes/Answers.js'
 
 const app = express();
 dotenv.config();
